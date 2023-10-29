49b858b21727ae74b3e5013a97c7f7a95b7d3460
husan <ibragimovdev@tstu.uz>
2023-10-29 13:07:54 +0500

    bugs fixed

--- eslatback/eslat/api/serializers.py
+++ eslatback/eslat/api/serializers.py
@@ -1,6 +1,5 @@
 import datetime
 from datetime import datetime
-from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from ..models import BotUser, Target, DailyTarget, FailPlan
 
@@ -18,19 +17,31 @@ class DailyTargetSerializer(serializers.ModelSerializer):
 
 
 class TargetSerializer(serializers.ModelSerializer):
+    weekdays_name = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_weekdays_name(obj):
+        return [weekday.weekday for weekday in obj.weekday.all()]
+
     class Meta:
         model = Target
-        fields = ['id', 'status', 'name', 'start_date', 'end_date', 'weekday', 'time', 'user']
+        fields = ['id', 'status', 'name', 'description', 'start_date', 'end_date', 'weekday', 'weekdays_name', 'time',
+                  'user']
+        extra_kwargs = {
+            'weekdays_name': {'write_only': True}
+        }
 
 
 class DailyTaskSerializer(serializers.ModelSerializer):
     day = serializers.SerializerMethodField()
     date = serializers.SerializerMethodField()
 
-    def get_date(self, obj):
+    @staticmethod
+    def get_date(obj):
         return datetime.now().date()
 
-    def get_day(self, obj):
+    @staticmethod
+    def get_day(obj):
         return datetime.now().strftime("%A").lower()
 
     class Meta:
